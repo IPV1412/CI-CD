d38ac3e638a3085c00bff6fbedc1abbc5962fea5
IPV1412 <115041634+IPV1412@users.noreply.github.com>
2025-12-11 11:18:47 -0600

    Almacenamiento de los números en un vector
    y muestra de estadísticas

--- Random.cpp
+++ Random.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <cstdlib>
 #include <ctime>
 
@@ -6,13 +7,31 @@ int main() {
     std::srand(std::time(0));
 
     int maxRange;
-    std::cout << "Ingresa el limite maximo de los numeros aleatorios: ";
+    std::cout << "Ingresa el limite maximo: ";
     std::cin >> maxRange;
 
+    std::vector<int> nums;
+    int sum = 0;
+
     for (int i = 0; i < 10; i++) {
-        int num = std::rand() % maxRange; 
-        std::cout << num << std::endl;
+        int num = std::rand() % maxRange;
+        nums.push_back(num);
+        sum += num;
     }
+
+    int min = nums[0];
+    int max = nums[0];
+
+    for (int n : nums) {
+        if (n < min) min = n;
+        if (n > max) max = n;
+    }
+
+    std::cout << "Numeros generados: ";
+    for (int n : nums) std::cout << n << " ";
+    std::cout << "\nMin: " << min
+              << "\nMax: " << max
+              << "\nPromedio: " << (sum / 10.0) << std::endl;
 
     return 0;
 }
